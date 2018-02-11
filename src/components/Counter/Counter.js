57215e59bf00a333dad5e686c5183ee6e46f97de
Charles <regalferg@gmail.com>
2018-02-10 21:36:13 -0500

    2nd working version

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -7,7 +7,7 @@ const Counter = props=>
  (
       <div id="main">
       <nav className="navbar"><ul><li className="brand">
-      <a href="/">Clicky Game</a></li><li className="">Click an image to begin!</li>
+      <a href="/clicky-react/">Clicky Game</a></li><li className="">Click an image to begin!</li>
       <li>Score: {props.count} | Top Score:{props.top}</li></ul></nav>
     
      
